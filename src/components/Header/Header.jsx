dc435b3431a60667834f5b5a3b8fbc3f889f4120
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-10-27 07:24:44 -0400

    update to stying

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const linkStyle = { fontSize: '28px' };
+  const brandStyle = { fontSize: '34px' };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Gil Rosario's Professional Site </Link>
+      <Link className="navbar-brand" to="/">
+        <span style={brandStyle}>Gil Rosario's Professional Site</span>
+      </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/" style={linkStyle}>Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/portfolio">Portfolio</Link>
+            <Link className="nav-link" to="/portfolio" style={linkStyle}>Portfolio</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
+            <Link className="nav-link" to="/contact" style={linkStyle}>Contact</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/resume">Resume</Link>
+            <Link className="nav-link" to="/resume" style={linkStyle}>Resume</Link>
           </li>
         </ul>
       </div>
@@ -29,3 +34,4 @@ function Header() {
 }
 
 export default Header;
+
