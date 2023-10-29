dc9d6113fd4deb1abb3fd5d25767221eb23c4810
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-10-28 22:15:14 -0400

    update styling in About me page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './AboutMe.css';
 
 function AboutMe() {
+  const [text, setText] = useState('');
+  const experienceText = "An experienced IT Engineer and current Fullstack Web development student seeking software engineering role opportunities. I aspire to work in a dynamic environment as a Software Engineer where I can use my analytical skills, current technical skills, and creative thinking abilities to create robust solutions. I am looking to secure a position that allows me to leverage my current technical infrastructure experience, maximize operational efficiencies, and exceed expectations. I aim to integrate my abilities by collaborating with cross-functional teams to translate highly complex system analysis within a diverse and comprehensive business environment.";
+
+  useEffect(() => {
+    let charIndex = 0;
+    const interval = setInterval(() => {
+      if (charIndex < experienceText.length) {
+        setText(experienceText.substring(0, charIndex + 1));
+        charIndex++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="container text-center mt-5">
-      <h1>Full Stack Web Developer</h1>
+      <h1 style={{ fontSize: '32px' }}>Full Stack Web Developer</h1>
       <div className="mx-auto my-5">
-        <img
-          src="/assets/gil-profile-pic.jpg"
-          alt="Gil Rosario"
-          className="img-fluid"
-          style={{ maxWidth: "130px" }}
-        />
+        <div className="d-flex justify-content-start align-items-center">
+          <img
+            src="/assets/gil-profile-pic.jpg"
+            alt="Gil Rosario"
+            className="img-fluid with-border-and-shadow"
+            style={{ maxWidth: "130px" }}
+          />
+          <p className="ml-4 text-left typing-text" style={{ fontSize: '18px', lineHeight: '1.5', padding: '40px' }}>
+            <p className="chalkduster-text">
+              Hello!... I am Gil Rosario
+            </p>
+            <br /> 
+            <p className="experience-text">
+              {text}
+            </p>
+          </p>
+        </div>
       </div>
-      <p className="text-left">
-        I am Gil Rosario, an experienced IT Engineer and current Fullstack Web development student seeking software engineering role opportunities. I aspire to work in a dynamic environment as a Software Engineer where I can use my analytical skills, current technical skills, and creative thinking abilities to create robust solutions. I am looking to secure a position that allows me to leverage my current technical infrastructure experience, maximize operational efficiencies, and exceed expectations. I aim to integrate my abilities by collaborating with cross-functional teams to translate highly complex system analysis within a diverse and comprehensive business environment.
-      </p>
     </section>
   );
 }
 
 export default AboutMe;
-
